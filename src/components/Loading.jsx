53afcf5747b0f4ce78dfdaee4502575e453bf1d4
tursynbekoff <talgat.tursynbekov@gmail.com>
2022-09-15 14:50:18 +0200

    big contentful shift

---
+++ src/components/Loading.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const mockList = [0,1,2,3,4,5,6,7,8,9];
+
+const LoadingView = () => {
+
+  return (
+    <div className="result">
+      <h2 className="result-text">Loading ...</h2>
+      <div className={`card-wrapper false-list`}>
+        {
+          mockList.map((item) => {
+            return (
+              <div className={`card card__false-list`}>
+                {
+                  <img src={`https://res.cloudinary.com/tursynbekoff/image/upload/w_50/v1662125174/no-image-1024x1024.png`}/>
+                }
+                <h3 className="title">
+                  {" "}
+                </h3>
+                <p className="author">
+                  {" "}
+                </p>
+
+                <p className="publish-year">
+                  {" "}
+                </p>
+                <p className="isbn">
+                  {" "}
+                </p>
+
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+    
+    
+  )
+}
+
+export default LoadingView;
